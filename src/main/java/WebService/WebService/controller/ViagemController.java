fc1ec40149f87e70b3c30be973c295293c9486cd
Rossana Dullius <67810701+RossanaD@users.noreply.github.com>
2021-06-11 09:07:12 -0300

    Add files via upload

--- src/main/java/WebService/WebService/controller/ViagemController.java
+++ src/main/java/WebService/WebService/controller/ViagemController.java
@@ -3,16 +3,20 @@ package WebService.WebService.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import WebService.WebService.dao.AviaoDAO;
 import WebService.WebService.dao.RotaDAO;
 import WebService.WebService.dao.ViagemDAO;
+import WebService.WebService.entity.Aviao;
+import WebService.WebService.entity.Rota;
 import WebService.WebService.entity.Viagem;
 
 @RestController
@@ -28,12 +32,13 @@ public class ViagemController {
 	private ViagemDAO viagemDAO;
 	
 	
-	  @PostMapping(value = "/addViagem") 
-	  public String postViagem(@RequestBody Viagem viagem) { 
-//		  Rota rota = rotaDAO.getById(idRota); 
-//		  Aviao aviao = aviaoDAO.getAviaoById(idAviao); 
-//		  Viagem viagem = new Viagem(); 
-//		  viagem.setRota(rota); viagem.setAviao(aviao);
+	  @PostMapping(value = "/addViagem", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}) 
+	  public String postViagem(@RequestParam("idAviao") int idAviao, @RequestParam("idRota") int idRota) { 
+		  Rota rota = rotaDAO.getById(idRota); 
+		  Aviao aviao = aviaoDAO.getAviaoById(idAviao); 
+		  Viagem viagem = new Viagem(); 
+		  viagem.setRota(rota); 
+		  viagem.setAviao(aviao);
 		  viagemDAO.addViagem(viagem); 
 	  return "Inserido com sucesso"; }
 	 
